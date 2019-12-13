7d0e171c46c61f183558dff97f3b079f8085a5eb
Min Choi <choi.minhyung@gmail.com>
2019-12-13 17:21:30 -0600

    Minimal Github API Backup Code
    Not connected to webpage yet

--- static/script.js
+++ static/script.js
@@ -21,3 +21,30 @@ function toggle_signup(){
         document.getElementById("team_config").innerHTML="Join Team";
     }
 }
+
+// Working GitHub API Code Below If Discord Does Not Work
+const gitHubForm = document.getElementById('entername');
+gitHubForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let usernameInput = document.getElementById('nameentered');
+    let gitHubUsername = usernameInput.value;
+    requestUserRepos(gitHubUsername);
+})
+function requestUserRepos(username){
+    const api_get = new XMLHttpRequest();
+    const url = `https://api.github.com/users/${username}/repos`;
+    api_get.open('GET', url, true);
+    api_get.onload = function () {
+        const data = JSON.parse(this.response);
+        for (let i in data) {
+            let ul = document.getElementById('repolist');
+            let li = document.createElement('li');
+            li.classList.add('list-group-item')
+            li.innerHTML = (`<p><strong>Repo:</strong> ${data[i].name}</p>
+                <p><strong>Description:</strong> ${data[i].description}</p>
+                <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>`);
+            ul.appendChild(li);
+        }
+    }
+    api_get.send();
+}
