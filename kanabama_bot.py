161a954637fc818943a02856b254d403ff09516e
Min Choi <choi.minhyung@gmail.com>
2019-12-13 17:49:51 -0600

    Adjusted

--- kanabama_bot.py
+++ kanabama_bot.py
@@ -21,6 +21,10 @@ client = commands.Bot(command_prefix=bot_prefix)
 @client.event
 async def on_ready():
     print("Kanabama Bot Running")
-    await client.change_presence(status=discord.Status.idle,activity=discord.Game(name="Prefix: *"))
+    await client.change_presence(status=discord.Status.idle,activity=discord.Game(name="Prefix: kana"))
 
-client.run("NjU1MTc2MDM1ODg4NzkxNTY3.XfQb3g.fInCvwdnY9U38t7CIGXk6RlaeMw")
+@client.command(pass_context=True)
+async def activate(ctx, member: discord.Member):
+    await client.send_message(member, 'boop')
+
+client.run("NjU1MTc2MDM1ODg4NzkxNTY3.XfQiOA.oCEDbv8H_a6mXbTT9VeFH1HUrWI")
