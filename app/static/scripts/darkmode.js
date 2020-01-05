7e40489ccb8cab3c1ca83a30f26d8b2be97be8a9
choix698 <choix698@umn.edu>
2020-01-04 23:07:46 -0600

    Dark Theme created, stupid toggle doesn't work

--- app/static/scripts/darkmode.js
+++ app/static/scripts/darkmode.js
@@ -2,4 +2,13 @@ dark_mode = false;
 
 function change_mode(){
   console.log("Changed!");
+  if (dark_mode==false) {
+    var link = document.getElementById("lightdarkstoryboard"); //Fetch the link by its ID
+    link.setAttribute("href", "{{ url_for('static', filename='styles/storystyle.css') }}"); //Change its href attribute
+    dark_mode = true;
+  } else {
+    var link = document.getElementById("lightdarkstoryboard"); //Fetch the link by its ID
+    link.setAttribute("href", "{{ url_for('static', filename='styles/dark-storystyle.css') }}"); //Change its href attribute
+    dark_mode = false;
+  }
 }
